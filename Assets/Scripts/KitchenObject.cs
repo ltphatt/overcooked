18ae38b6544966e7a5b2d84c9680c5558ad44394
Lê Thanh Phát <ltphatt2709@gmail.com>
2024-09-06 07:30:48 +0700

    feat: add kitchen object parent

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    private ClearCounter clearCounter;
 
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter _clearCounter)
+    {
+        // Clear kitchen object from old counter
+        if (clearCounter != null)
+        {
+            clearCounter.ClearKitchenObject();
+        }
+
+        // Add new clear counter
+        clearCounter = _clearCounter;
+        if (_clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter has already kitchen object!");
+        }
+        _clearCounter.SetKitchenObject(this);
+
+        // Update visual
+        transform.parent = clearCounter.GetCounterTopPoint();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
