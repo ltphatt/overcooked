248810671150aae19ba4ba6a9345768882c58914
Lê Thanh Phát <ltphatt2709@gmail.com>
2024-09-09 07:11:33 +0700

    feat: pick up and drop kitchen obj

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,7 +8,31 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent
 
     public override void Interact(Player player)
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        if (!HasKitchenObject())
+        {
+            // There is no kitchen obj here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player not carrying anything
+            }
+        }
+        else
+        {
+            // There is a kitchen obj here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying sth
+            }
+            else
+            {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
