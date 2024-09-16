fd4aa2ec0ebdd175cf727b3734b090fa45d44fe5
Lê Thanh Phát <ltphatt2709@gmail.com>
2024-09-16 07:49:35 +0700

    feat: add plates counter

---
+++ Assets/Scripts/Counters/PlatesCounterVisual.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounterVisual : MonoBehaviour
+{
+    [SerializeField] private PlatesCounter platesCounter;
+    [SerializeField] private Transform CounterTopPoint;
+    [SerializeField] private Transform PlateVisualPrefab;
+    private List<GameObject> plateVisualGameObjectList;
+
+    private void Awake()
+    {
+        plateVisualGameObjectList = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+        platesCounter.OnPlateRemoved += PlatesCounter_OnPlateRemoved;
+
+    }
+
+    private void PlatesCounter_OnPlateRemoved(object sender, EventArgs e)
+    {
+        GameObject plateGameObject = plateVisualGameObjectList[plateVisualGameObjectList.Count - 1];
+        plateVisualGameObjectList.Remove(plateGameObject);
+        Destroy(plateGameObject);
+    }
+
+    private void PlatesCounter_OnPlateSpawned(object sender, EventArgs e)
+    {
+        Transform plateVisualTransform = Instantiate(PlateVisualPrefab, CounterTopPoint);
+
+        // Plates spawned stack
+        float plateOffsetY = .1f;
+        plateVisualTransform.localPosition = new Vector3(0, plateOffsetY * plateVisualGameObjectList.Count, 0);
+        plateVisualGameObjectList.Add(plateVisualTransform.gameObject);
+    }
+}
